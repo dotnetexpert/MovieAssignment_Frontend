c2f4d34fa6e1a22533b963238171c4ebb7330935
dotnetexpert <expert.dnet@gmail.com>
2024-10-28 13:55:47 +0530

    Update apiServices.js

--- src/utils/apiServices.js
+++ src/utils/apiServices.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3020/api", // Adjust the base URL to your API
+    baseURL: process.env.REACT_APP_BACKEND_URL, // Adjust the base URL to your API
 });
 
 // Request interceptor
